9a5bdd910e1179458ab325b5bb275e1af23c6a4e
Priyam <priyamvada7302@gmail.com>
2026-09-06 22:24:58 +0530

    Comment out eventbridge rules in eventbridge-changes.py

---
+++ eventbridge-changes.py
@@ -0,0 +1,10 @@
+# eventbridge rules changed
+
+# {
+#   "source": ["aws.iam", "aws.ec2"],
+#   "detail-type": ["AWS API Call via CloudTrail"],
+#   "detail": {
+#     "readOnly": [false],
+#     "eventName": ["CreateAccessKey", "CreateUser", "AttachUserPolicy", "AttachGroupPolicy", "PutUserPolicy", "CreateLoginProfile", "DeleteTrail", "StopLogging", "UpdateAssumeRolePolicy", "RunInstances", "StartInstances", "StopInstances", "TerminateInstances", "ModifySecurityGroupRules", "CreateSnapshot", "AuthorizeSecurityGroupIngress", "AuthorizeSecurityGroupEgress", "RevokeSecurityGroupIngress", "RevokeSecurityGroupEgress", "ModifySecurityGroupRules"]
+#   }
+# }
